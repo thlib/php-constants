42ce1fe7c9321dc2c3fb125705d44bdd3f4a9ff7
Timo Huovinen <timoh89@gmail.com>
2020-04-09 17:36:17 +0200

    init

---
+++ src/ContentType.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace THLib;
+
+class ContentType 
+{
+    const TEXT_PLAIN = 'text/plain;charset=utf-8';
+    const TEXT_HTML = 'text/html;charset=utf-8';
+    const APPLICATION_JSON = 'application/json';
+    const APPLICATION_FORM_URLENCODED = 'application/x-www-form-urlencoded';
+    const IMAGE_JPG = 'image/jpeg';
+    const IMAGE_PNG = 'image/png';
+    const IMAGE_GIF = 'image/gif';
+}
