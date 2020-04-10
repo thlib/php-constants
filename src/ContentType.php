82615a4ec3aae332ea314741d8d225df7f4bdf56
Timo Huovinen <timoh89@gmail.com>
2020-04-10 15:53:57 +0200

    xml content type

--- src/ContentType.php
+++ src/ContentType.php
@@ -6,6 +6,7 @@ class ContentType
 {
     const TEXT_PLAIN = 'text/plain;charset=utf-8';
     const TEXT_HTML = 'text/html;charset=utf-8';
+    const TEXT_XML = 'text/xml;charset=utf-8';
     const APPLICATION_JSON = 'application/json';
     const APPLICATION_FORM_URLENCODED = 'application/x-www-form-urlencoded';
     const IMAGE_JPG = 'image/jpeg';
